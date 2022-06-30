157fddde2647646bb556eb725032230fdb5a0b5b
Daniel <danielnatsufire@gmail.com>
2022-06-30 01:43:16 -0300

    tentando deploy

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ import { MongoClient } from "mongodb"
 import dotenv from 'dotenv'
 
 dotenv.config()
-const mongoClient = new MongoClient(process.env.MONGO_URL)
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 
 export default mongoClient
