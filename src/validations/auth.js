bf42f730c0520ac5c582ea2cba6802b458fa3f64
Daniel <danielnatsufire@gmail.com>
2022-06-29 22:05:47 -0300

    fix: mensagens das validations

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -2,26 +2,53 @@ import Joi from 'joi'
 
 const userSchema = Joi.object({
     name: Joi.string()
+        .messages({
+            "string.empty": `"name" não pode ser vazio`,
+            "any.required": `"name" é obrigatório!`
+        })
         .required(),
     email: Joi.string()
         .pattern(/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/)
-        .message('"email" must have @ character and .')
+        .messages({
+            "string.pattern.base": `"email" precisa conter @ e um . ex: @example.com`,
+            "string.empty": `"email" não pode ser vazio`,
+            "any.required": `"email" é obrigatório!`
+        })
         .required(),
     password: Joi.string()
         .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%&*]{8,}$/)
-        .message('"password" must have at least one uppercase letter, one number and at least 8 characters')
+        .message({
+            "string.pattern.base": `"password" precisa conter 1 letra maiuscula, 1 numero e pelo menos 8 caracteres`,
+            "string.empty": `"password" não pode ser vazio`,
+            "any.required": `"password" é obrigatório!`
+        })
         .required(),
-    confirmPassword: Joi.ref('password')
+    confirmPassword: Joi.any()
+        .messages({
+            "any.only": `"confirmPassword" precisa ser igual a senha passada`,
+            "string.empty": `"confirmPassword" não pode ser vazio`,
+            "any.required": `"confirmPassword" é obrigatório!`
+        })
+        .valid(Joi.ref('password'))
+        .required()
 })
 
 const loginSchema = Joi.object({
     email: Joi.string()
         .pattern(/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/)
-        .message('"email" must have @ character and .')
+        .messages({
+            "string.pattern.base": `"email" precisa conter @ e um . ex: @example.com`,
+            "string.empty": `"email" não pode ser vazio`,
+            "any.required": `"email" é obrigatório!`
+        })
         .required(),
     password: Joi.string()
         .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%&*]{8,}$/)
-        .message('"password" must have at least one uppercase letter, one number and at least 8 characters')
+        .message({
+            "string.pattern.base": `"password" precisa conter 1 letra maiuscula, 1 numero e pelo menos 8 caracteres`,
+            "string.empty": `"password" não pode ser vazio`,
+            "any.required": `"password" é obrigatório!`
+        })
         .required()
 })
 
--- src/validations/record.js
+++ src/validations/record.js
@@ -2,10 +2,22 @@ import Joi from 'joi'
 
 const recordSchema = Joi.object({
     price: Joi.number()
+        .messages({
+            "number.empty": `"price" não pode ser vazio`,
+            "any.required": `"price" é obrigatório!`
+        })
         .required(),
     description: Joi.string()
+        .messages({
+            "string.empty": `"description" não pode ser vazio`,
+            "any.required": `"description" é obrigatório!`
+        })
         .required(),
     isIncrease: Joi.bool()
+        .messages({
+            "boolean.base": `"isIncrease" precisa ser booleano`,
+            "any.required": `"isIncrease" é obrigatório!`
+        })
         .required(),
     date: Joi.string()
         .required(),
